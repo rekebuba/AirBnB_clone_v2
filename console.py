44e57f248db476a07ddbfb52823eb6d108b0c31a
rekebuba <abukiabdullahi1@gmail.com>
2024-03-06 23:57:50 +0300

    AirBnB_clone

--- console.py
+++ console.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import cmd
+from models.base_model import BaseModel 
 
 
 class HBNBCommand(cmd.Cmd):
@@ -11,6 +12,28 @@ class HBNBCommand(cmd.Cmd):
     def do_quit(self, args):
         """Used to exit the program"""
         return True
+    def do_create(self, args):
+        """Creates a new instance of BaseModel"""
+        if not args:
+            print("** class name missing **")
+        elif args == "BestModel":
+            instance = BaseModel()
+            instance.save()
+            print(instance.id)
+        else:
+            print("** class doesn't exist **")
+
+    def do_show(self, args):
+        """Prints the string representation of an instance based on the class name and id"""
+        lists = [arg for arg in args.split()]
+        if not lists[0]:
+            print("** class name is missing **")
+        elif lists[0] == "BestModel" and lists[1]:
+            ...
+            print(args)
+
+
+
 
 
 if __name__ == '__main__':
