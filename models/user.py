d689c1303117a92b6cee745df0108fcb3f78775f
rekebuba <abukiabdullahi1@gmail.com>
2024-03-08 00:31:21 +0300

    AirBnB_clone

--- models/user.py
+++ models/user.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+from models.base_model import BaseModel
+from models import storage
+
+class User(BaseModel):
+    """a class User that inherits from BaseModel"""
+    def __init__(self):
+        self.email = ""
+        self.password = ""
+        self.first_name = ""
+        self.last_name = ""
+        self.id = BaseModel().id
+        
