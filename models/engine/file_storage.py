a6ff808b0ceaf74d33bacbe0e7a441bd0f876dd5
rekebuba <abukiabdullahi1@gmail.com>
2024-03-08 11:01:23 +0300

    AirBnB_clone

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -27,9 +27,20 @@ class FileStorage:
     def Classes(self):
         from models.base_model import BaseModel
         from models.user import User
+        from models.amenity import Amenity
+        from models.city import City
+        from models.place import Place
+        from models.review import Review
+        from models.state import State
+
         classes = {
             "BaseModel": BaseModel,
-            "User": User
+            "User": User,
+            "Amenity": Amenity,
+            "City": City,
+            "Place": Place,
+            "Review": Review,
+            "State": State
         }
         return classes
 
