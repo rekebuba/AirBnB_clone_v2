c21316d972477ce399a2ff512e6862d888b40382
rekebuba <abukiabdullahi1@gmail.com>
2024-04-19 15:30:17 +0300

    AirBnB_clone_v2

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -26,9 +26,7 @@ class FileStorage:
 
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
-        self.__objects.update(
-            {obj.to_dict()['__class__'] + '.' + obj.id: obj}
-            )
+        self.__objects[f'{obj.__class__.__name__}.{obj.id}'] = obj
 
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
