c1eb31caf830252abe2960d06f522403182bccdf
rekebuba <abukiabdullahi1@gmail.com>
2024-04-19 17:31:07 +0300

    AirBnB_clone_v2

--- models/amenity.py
+++ models/amenity.py
@@ -11,4 +11,5 @@ class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
     if storage_type == 'db':
         name = Column(String(128), nullable=False)
-    name = ""
+    else:
+        name = ""
