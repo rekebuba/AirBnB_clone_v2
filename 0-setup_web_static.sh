5ac1cd1168c33e2b54f9c0cf0d853271ece1f4be
rekebuba <abukiabdullahi1@gmail.com>
2024-05-04 01:05:00 +0300

    AirBnB clone - Deploy static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,3 +1,57 @@
 #!/usr/bin/env bash
 
+if ! command -v nginx -v &> /dev/null; then
+    sudo apt -y update
+    sudo apt -y install nginx
+fi
 
+if ! command -v cd data/ &> /dev/null; then
+    mkdir data
+fi
+
+if ! command -v cd data/web_static/ &> /dev/null; then
+    mkdir data/web_static
+fi
+
+if ! command -v cd data/web_static/releases/ &> /dev/null; then
+    mkdir data/web_static/releases
+fi
+
+if ! command -v cd data/web_static/shared/ &> /dev/null; then
+    mkdir data/web_static/shared
+fi
+
+if ! command -v cd data/web_static/shared/ &> /dev/null; then
+    mkdir data/web_static/shared
+fi
+
+if ! command -v cd data/web_static/releases/test/ &> /dev/null; then
+    mkdir data/web_static/releases/test
+    code="<html>
+    <head>
+    </head>
+    <body>
+        Holberton School
+    </body>
+    </html>
+        "
+    touch data/web_static/releases/test/index.html
+    echo "$code" | tee data/web_static/releases/test/index.html
+fi
+
+if ! command -v cd data/web_static/current/ &> /dev/null; then
+    mkdir data/web_static/current
+fi
+chmod 777 -R data/
+ln -s -f data/web_static/releases/test/ data/web_static/current
+
+server_config=\
+'
+        locarion /current/ {
+            alias /data/web_static/current/;
+            autoindex off;
+        }
+'
+if ! grep -qF "$server_config" /etc/nginx/sites-available/default; then
+    sudo sed -i '55 r /dev/stdin' /etc/nginx/sites-available/default <<< "$server_config"
+fi
