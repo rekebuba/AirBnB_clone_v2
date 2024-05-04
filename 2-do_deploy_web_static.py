49f9dcb212f5c24fbe7bf15cf0c2d767c595ad9b
rekebuba <abukiabdullahi1@gmail.com>
2024-05-04 22:56:01 +0300

    AirBnB clone - Deploy static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -14,18 +14,20 @@ def do_deploy(archive_path):
     A Fabric script that distributes an archive to
     web servers, using the function do_deploy
     """
-    if os.path.exists(archive_path):
-        try:
-            file_name = re.search(r"web_static_\d*", archive_path).group()
-            put("archive_path", "/temp/")
-            run(f"mkdir -p /data/web_static/releases/{file_name}")
-            run(f"tar -xzf /data/web_static/releases/{file_name}.tgz \
-                -C /data/web_static/releases/{file_name}")
-            run(f"rm /tmp/{file_name}.tgz")
-            run("rm /data/web_static/current")
-            run(f"ln -s -f /data/web_static/releases/{file_name} \
-                /data/web_static/current")
-        except Exception:
+    try:
+        if not os.path.exists(archive_path):
             return False
-    else:
+
+        file_name = re.search(r"web_static_\d*", archive_path).group()
+        put("archive_path", "/temp/")
+        run(f"mkdir -p /data/web_static/releases/{file_name}")
+        run(f"sudo tar -xzf /data/web_static/releases/{file_name}.tgz \
+            -C /data/web_static/releases/{file_name}")
+        run(f"sudo rm /tmp/{file_name}.tgz")
+        run("sudo rm -rf /data/web_static/current")
+        run(f"sudo ln -s -f /data/web_static/releases/{file_name} \
+            /data/web_static/current")
+    except Exception:
         return False
+
+    return True
