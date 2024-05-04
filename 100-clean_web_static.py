5df68580960c26c2f9793342f704a5d5d92646fc
rekebuba <abukiabdullahi1@gmail.com>
2024-05-05 02:24:38 +0300

    AirBnB clone - Deploy static

---
+++ 100-clean_web_static.py
@@ -0,0 +1,32 @@
+"""
+Fabric script (based on the file 3-deploy_web_static.py)
+"""
+from fabric.api import local, run
+
+
+env.hosts = ['54.175.224.158', '100.25.129.156']
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/id_rsa'
+
+
+def do_clean(number=0):
+    """
+    A Fabric script that deletes out-of-date archives,
+    using the function do_clean:
+    """
+    if number == 0:
+        number = 1
+
+    L_no_line = local(f"ls -c versions/web_static_* | wc -l")
+    L_paths = local(f"ls -c versions/web_static_* | tail -n \
+        {L_no_line - number}").split('\n')
+
+    for L_path in L_paths:
+        local(f"sudo rm -fr {path}")
+
+    R_no_line = run("ls -c /data/web_static/releases/web_static_* | wc -l")
+    R_paths = run(f"ls -c /data/web_static/releases/web_static_* | tail -n \
+        {R_no_line - number}").split('\n')
+
+    for R_path in R_paths:
+        run(f"sudo rm -fr {R_path}")
