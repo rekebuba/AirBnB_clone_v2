7fc294e5709c744f8b572e5cc38d46fdf7d279c4
rekebuba <abukiabdullahi1@gmail.com>
2024-05-04 18:21:41 -0700

    AirBnB clone - Deploy static

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -28,10 +28,9 @@ def do_clean(number=0):
             local(f"sudo rm -fr {L_path}")
 
     R_no_line = run("ls /data/web_static/releases/ | grep 'web_static_' | wc -l")
-    print(f"R_no_line: {R_no_line}")
     R_paths = run(f"ls -c /data/web_static/releases/ | grep 'web_static_' | tail -n \
         {int(R_no_line) - number}").split('\n')
-    print(f"R_paths: {R_paths}")
+
     for R_path in R_paths:
         if R_path != '':
-            run(f"sudo rm -fr {R_path}")
+            run(f"sudo rm -fr /data/web_static/releases/{R_path}")
