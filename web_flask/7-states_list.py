07fa217ff7a6e474c0c45a6768f97211dd65e9dc
rekebuba <abukiabdullahi1@gmail.com>
2024-05-18 13:39:04 +0300

    AirBnB clone - Web framework

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+from flask import Flask, render_template
+from models.state import State
+from models import storage
+
+app = Flask(__name__)
+
+@app.route('/states_list', strict_slashes=False)
+def state_list():
+    result = []
+    list_of_objects = list(storage.all(State).values())
+    for lists in list_of_objects:
+        result.append(lists.__dict__)
+    
+    return render_template('7-states_list.html', list_of_dict=result)
+
+@app.template_filter('sort_by_name')
+def sort_by_name(list_of_dict):
+    return sorted(list_of_dict, key=lambda x: x['name'])
+
+# @app.teardown_appcontext
+# def teardown_db():
+#     storage.close()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
